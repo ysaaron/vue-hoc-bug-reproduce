7011bbc4183e938ce9b95690c3a2b594cfb4ce60
AaronYoung <aaron.young@linecorp.com>
2018-04-18 23:52:57 +0900

    Remove condition detecting whethere node is text

--- vue-hoc/packages/vue-hoc/src/normalizeSlots.js
+++ vue-hoc/packages/vue-hoc/src/normalizeSlots.js
@@ -5,21 +5,13 @@ function isTextNode(node) {
 const normalizeSlots = (slots, context) => Object.keys(slots)
   .reduce((arr, key) => {
     let template = false;
-    slots[key].forEach(vnode => {
+    slots[key].forEach((vnode) => {
       if (!vnode.context) {
-        if (isTextNode(vnode)) {
-          if (!template) {
-            slots[key] = context.$createElement('template', {slot: key}, slots[key]);
-            template = true;
-          }
-        } else {
-          slots[key].context = context;
-
-          if (!vnode.data) {
-            vnode.data = {};
-          }
-          vnode.data.slot = key;
+        slots[key].context = context;
+        if (!vnode.data) {
+          vnode.data = {};
         }
+        vnode.data.slot = key;
       }
     });
     return arr.concat(slots[key]);
